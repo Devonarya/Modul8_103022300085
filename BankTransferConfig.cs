e3e83e0757c0f6649165b9d12f1575b033c063af
PRAKTIKAN <praktikan@gmail.com>
2025-04-17 11:13:01 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Modul8_103022300085
+{
+    public class BankTransferConfig
+    {
+        //Attribute untuk diserialisasi
+        public String lang { get; set; }
+        public transfer Transfer { get; set; }
+        public String [] methods { get; set; }
+        public confirmation Confirmation { get; set;}
+
+        public class transfer
+        { 
+            public int treshold { get; set;}          
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+
+        public class confirmation
+        { 
+            public string en { get; set;}
+            public string id { get; set;}
+        }
+        public BankTransferConfig() { 
+            lang = "en";
+            Transfer = new transfer();
+            Transfer.treshold = 25000000;
+            Transfer.low_fee = 6500;
+            Transfer.high_fee = 15000;
+            methods = new string[] { "RTO(real - time)", "SKN", "RTGS", "BI FAST" };
+            Confirmation = new confirmation();
+            Confirmation.en = "yes";
+            Confirmation.id = "ya";
+        }
+
+        public void readConfigFile(string path)
+        {
+
+            if (File.Exists(path))
+            {
+                string jsonText = File.ReadAllText(path);
+                BankTransferConfig config = JsonSerializer.Deserialize<BankTransferConfig>(jsonText);
+                this.lang = config.lang;
+                this.Transfer = config.Transfer;
+                this.methods = config.methods;
+                this.Confirmation = config.Confirmation;
+
+            }
+            else {
+                 Console.WriteLine("Gagal membaca file");
+            }
+        }
+
+        public void writeNewConfig(string path)
+        {
+            string jsonText = JsonSerializer.Serialize(this);
+            File.WriteAllText(path, jsonText);
+            Console.WriteLine("File Berhasil ditulis!");
+        }
+
+        public void readandwriteConfigFile(string path) {
+            if (File.Exists(path)) { 
+                readConfigFile(path);
+            } else
+            {
+                writeNewConfig(path);
+                readConfigFile(path);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using Modul8_103022300085;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.Json;
+public class program {
+    public static void Main(String[] args)
+    {
+        BankTransferConfig bankTransferConfig = new BankTransferConfig();
+        string configPath = "bank_transfer_config.json";
+        bankTransferConfig.readConfigFile(configPath);
+
+        if (bankTransferConfig.lang == "en") {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        } else if(bankTransferConfig.lang == "id") { 
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+
+        int nominalTransfer = int.Parse(Console.ReadLine());
+        int totalBiaya = 0;
+
+        if (nominalTransfer <= bankTransferConfig.Transfer.treshold)
+        {
+            totalBiaya = bankTransferConfig.Transfer.low_fee + nominalTransfer;
+        }
+        else { 
+            totalBiaya = bankTransferConfig.Transfer.high_fee + nominalTransfer;
+        }
+
+        if (bankTransferConfig.lang == "en")
+        {
+            Console.WriteLine($"Transfer Fee = {totalBiaya - nominalTransfer}");
+            Console.WriteLine($"Total amount = {totalBiaya}");
+            Console.WriteLine("select transfer method : ");
+        }
+        else if (bankTransferConfig.lang == "id") {
+            Console.WriteLine($"Transfer Fee = {totalBiaya - nominalTransfer}");
+            Console.WriteLine($"Total amount = {totalBiaya}");
+            Console.WriteLine("Pilih Transfer Method : ");
+        }
+
+
+    }
+}
